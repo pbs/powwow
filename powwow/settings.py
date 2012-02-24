b98a50ae18cbf3df89f1fbd988a2a423fd09c217
Angel Ramboi <angel.ramboi@gmail.com>
2012-02-24 16:00:28 +0200

    Modified imported apps to contain project name.

--- powwow/settings.py
+++ powwow/settings.py
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'south',
-    'apps',
+    'powwow.apps',
 )
 
 # A sample logging configuration. The only tangible logging
