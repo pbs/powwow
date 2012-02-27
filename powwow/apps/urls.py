195b1ea44ccb02157a07d791d2c27c8266120b02
Angel Ramboi <angel.ramboi@gmail.com>
2012-02-27 10:54:47 +0200

    Added bootstrap css and js. Added dev env xml.

--- powwow/apps/urls.py
+++ powwow/apps/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('powwow.apps.views',
     url(r'^app.xml$', 'index', name='index'),
+    url(r'^app_dev.xml$', 'index_dev', name='index_dev'),
 )
--- powwow/apps/views.py
+++ powwow/apps/views.py
@@ -4,4 +4,7 @@ from powwow.apps.models import AppSettings
 
 
 def index(request):
-    return render_to_response('app.xml')
+    return render_to_response('app.xml', {request: request})
+
+def index_dev(request):
+    return render_to_response('app_dev.xml', {request: request})
